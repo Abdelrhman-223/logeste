7b8279d1fb15691abf2595f99acb7708d2bb37be
abdo <abdelrhman@DESKTOP-G0SHSIH.(none)>
2023-01-27 00:06:37 +0200

    Add fonts and control colors

---
+++ lib/core/utils/fonts.dart
@@ -0,0 +1,13 @@
+class AppFonts {
+
+  static String appFontFamily = "Cairo";
+
+  static double myH1 = 50;
+  static double myH2 = 45;
+  static double myH3 = 40;
+  static double myH4 = 30;
+  static double myH5 = 25;
+  static double myH6 = 20;
+  static double myP = 18;
+
+}
